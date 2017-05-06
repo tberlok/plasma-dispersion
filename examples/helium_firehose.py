77e924289e89fe13dff1afcbbdbd47b869d0a4e8
Thomas Berlok <berlok@nbi.dk>
2017-05-06 23:00:08 +0200

    Added helium-hydrogen firehose
    just for the fun of it

---
+++ examples/helium_firehose.py
@@ -0,0 +1,47 @@
+from vlasov import Species, Electrons, VlasovFluid
+from vlasov.cython.types import Complex
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import newton
+
+# Magnetic field strength
+B0 = 1
+# Abundances
+nH = 0.8
+nHe = 0.2
+ne = nH + 2*nHe
+hydrogen = Species(Tpar=2, Tperp=1/2, n0=nH, mass=1, charge=1)
+helium = Species(Tpar=2, Tperp=1/2, n0=nHe, mass=4, charge=2)
+species = (hydrogen, helium)
+# Electron fluid properties
+electrons = Electrons(Te=1/2, ne=ne)
+
+# Initialize solver
+vlasov = VlasovFluid(B0, species, electrons)
+
+Nk = 400
+kpar = np.linspace(1e-2, 2, Nk)
+kperp = 1e-8
+
+omega = np.zeros(Nk, dtype=Complex)
+
+# Guess for first solution
+omega[0] = 1 + 1j
+
+# Get the first root
+omega[0] = newton(vlasov.det, omega[0], args=(kpar[0], kperp))
+
+for i in range(1, Nk):
+    omega[i] = newton(vlasov.det, omega[i-1], args=(kpar[i], kperp))
+
+# Plotting
+fig, axes = plt.subplots(nrows=2)
+axes[1].plot(kpar, omega.real, '-')
+axes[0].plot(kpar, omega.imag, '-')
+axes[1].set_ylabel(r"$Re(\omega/\Omega)$")
+axes[0].set_ylabel(r"$Im(\omega/\Omega)$")
+plt.xlim(0, 2)
+plt.xlabel(r"$k_\parallel v_a/\Omega$")
+# plt.savefig("../figures/parallel_firehose.eps")
+print("Maximum growth rate is:", omega.imag.max())
+plt.show()
