cf6ad98607af7d5127ce07dd0ea5a99c514c4fb7
Thomas Berlok <berlok@nbi.dk>
2017-06-01 15:31:28 +0200

    Attempt at using derivative of sigma for Newton
    For some reason this does not give the improve in
    performance that I had been hoping for.

--- vlasov/cython/lamb.pyx
+++ vlasov/cython/lamb.pyx
@@ -69,3 +69,76 @@ def lambda_tensor(complex_t omega, real_t kpar, real_t kperp, species_t s):
     a[8] -= 1/(1-s.Delta)*zeta_0**2
 
     return a
+
+
+def derivative_lambda_tensor(complex_t omega, real_t kpar, real_t kperp,
+                             species_t s):
+
+    cdef int N = 1000
+    cdef real_t tol = 1e-16
+    cdef int n, count, i, j, sign
+    cdef complex_t T[9]
+    cdef complex_t dT[9]
+    cdef complex_t a[9]
+    cdef complex_t zeta_0, zeta_n, square, d_square, dW, Wn, temp, d_zeta_n
+    cdef real_t Gam, GamP, lamb, lamb_sqrt
+
+    lamb_sqrt = kperp*s.vtp/s.oc
+    lamb = lamb_sqrt**2.0
+    zeta_0 = omega/(kpar*s.vt)
+
+    d_zeta_n = 1.0/(kpar*s.vt)
+
+    for i in range(9):
+        a[i] = 0.0
+
+    # This loop has n = 0, 1, -1, 2, -2, ... N, - N
+    sign = -1
+    for j in range(1, 2*(N+1)):
+        n = j/2*sign
+        sign *= -1
+        zeta_n = (omega - n*s.oc)/(kpar*s.vt)
+        Wn = w_func(zeta_n)
+        Gam = ive(n, lamb)
+        GamP = 0.5*(ive(n-1, lamb) + ive(n+1, lamb)) - ive(n, lamb)
+        dW = -(zeta_n*Wn + (Wn + 1.0)/zeta_n)
+
+        square = omega/(omega-n*s.oc)*(1.0-Wn) + s.Delta*Wn
+        d_square = -(1.0 - Wn)*n*s.oc/(omega - n*s.oc)**2.0
+        d_square += (s.Delta - omega/(omega - n*s.oc))*dW/(kpar*s.vt)
+        T[0] = n**2*Gam/lamb
+        T[1] = 1j*n*GamP
+        T[2] = n*Gam/sqrt(lamb)*zeta_n/sqrt(1-s.Delta)
+        T[3] = T[1].conjugate()
+        T[4] = n**2*Gam/lamb - 2*GamP*lamb
+        T[5] = -1j*sqrt(lamb)*GamP*zeta_n/sqrt(1-s.Delta)
+        T[6] = T[2].conjugate()
+        T[7] = T[5].conjugate()
+        T[8] = Gam*zeta_n**2/(1-s.Delta)
+
+        dT[0] = 0.0
+        dT[1] = 0.0
+        dT[2] = n*Gam/sqrt(lamb)*d_zeta_n/sqrt(1-s.Delta)
+        dT[3] = 0.0
+        dT[4] = 0.0
+        dT[5] = -1j*sqrt(lamb)*GamP*d_zeta_n/sqrt(1-s.Delta)
+        dT[6] = T[2].conjugate()
+        dT[7] = T[5].conjugate()
+        dT[8] = Gam*2.0*d_zeta_n*zeta_n/(1-s.Delta)
+
+        count = 0
+        for i in range(9):
+            temp = d_square*T[i] + square*dT[i]
+            if j > 1:
+                if abs(temp/a[i]) > tol:
+                    a[i] += temp
+                    count += 1
+            else:
+                a[i] += temp
+                count += 1
+        if count == 0 and n < 0:
+            break
+
+    a[8] -= 1/(1-s.Delta)*2.0*d_zeta_n*zeta_0
+
+    return a
--- vlasov/vlasovfluid.py
+++ vlasov/vlasovfluid.py
@@ -9,6 +9,8 @@ class VlasovFluid():
         self.kk = np.zeros((3, 3))
         self.sigma = np.zeros((3, 3), dtype=Complex)
         self.D = np.zeros((3, 3), dtype=Complex)
+        self.sigma_p = np.zeros((3, 3), dtype=Complex)
+        self.D_p = np.zeros((3, 3), dtype=Complex)
 
         # Magnetic field strength
         self.B0 = B0
@@ -57,6 +59,20 @@ class VlasovFluid():
 
         self.sigma = np.reshape(self.sigma, (3, 3))
 
+    def derivative_conductivity_tensor(self, omega, kpar, kperp):
+        from vlasov.cython.lamb import derivative_lambda_tensor
+        from vlasov.cython.lamb import lambda_tensor
+        self.sigma_p[:] = 0.0
+        self.sigma_p = np.reshape(self.sigma_p, 9)
+        for ion in self.species:
+            b = np.array(derivative_lambda_tensor(omega, kpar, kperp, ion))
+            self.sigma_p += 1j/omega*ion.charge**2*ion.n0/ion.mass * b
+        for ion in self.species:
+            b = np.array(lambda_tensor(omega, kpar, kperp, ion))
+            self.sigma_p += -1j/omega**2*ion.charge**2*ion.n0/ion.mass * b
+
+        self.sigma_p = np.reshape(self.sigma_p, (3, 3))
+
     def dispersion_tensor(self, omega, kpar, kperp):
         self.get_kk(kpar, kperp)
         self.get_k2(kpar, kperp)
@@ -66,6 +82,41 @@ class VlasovFluid():
         self.D += np.dot(self.mu0*self.sigma, self.electrons.dpedne
                          / self.electrons.charge*self.kk)
         self.D -= 1j*omega*self.mu0*self.electrons.ene*np.identity(3)
+
+    def derivative_dispersion_tensor(self, omega, kpar, kperp):
+        self.get_k2(kpar, kperp)
+        self.conductivity_tensor(omega, kpar, kperp)
+        self.derivative_conductivity_tensor(omega, kpar, kperp)
+        self.D_p = np.dot(1j*self.mu0*omega*self.sigma_p +
+                          1j*self.mu0*self.sigma, self.Bcross1)
+        self.D_p += np.dot(self.mu0*self.sigma_p, self.electrons.dpedne
+                           / self.electrons.charge*self.kk)
+        self.D_p -= 1j*self.mu0*self.electrons.ene*np.identity(3)
+
+    def derivative_det(self, omega, kpar, kperp):
+        self.derivative_dispersion_tensor(omega, kpar, kperp)
+        self.dispersion_tensor(omega, kpar, kperp)
+
+        f = -self.D[1, 2] * self.D[2, 1] * self.D_p[0, 0] + \
+            self.D[1, 1] * self.D[2, 2] * self.D_p[0, 0] + \
+            self.D[1, 2] * self.D[2, 0] * self.D_p[0, 1] - \
+            self.D[1, 0] * self.D[2, 2] * self.D_p[0, 1] - \
+            self.D[1, 1] * self.D[2, 0] * self.D_p[0, 2] + \
+            self.D[1, 0] * self.D[2, 1] * self.D_p[0, 2] + \
+            self.D[0, 2] * self.D[2, 1] * self.D_p[1, 0] - \
+            self.D[0, 1] * self.D[2, 2] * self.D_p[1, 0] - \
+            self.D[0, 2] * self.D[2, 0] * self.D_p[1, 1] + \
+            self.D[0, 0] * self.D[2, 2] * self.D_p[1, 1] + \
+            self.D[0, 1] * self.D[2, 0] * self.D_p[1, 2] - \
+            self.D[0, 0] * self.D[2, 1] * self.D_p[1, 2] - \
+            self.D[0, 2] * self.D[1, 1] * self.D_p[2, 0] + \
+            self.D[0, 1] * self.D[1, 2] * self.D_p[2, 0] + \
+            self.D[0, 2] * self.D[1, 0] * self.D_p[2, 1] - \
+            self.D[0, 0] * self.D[1, 2] * self.D_p[2, 1] - \
+            self.D[0, 1] * self.D[1, 0] * self.D_p[2, 2] + \
+            self.D[0, 0] * self.D[1, 1] * self.D_p[2, 2]
+
+        return f
 
     def det(self, omega, kpar, kperp):
         self.dispersion_tensor(omega, kpar, kperp)
